cd2f648b8694d98ea20426a63ed78a94b59fe77a
ohmpatel48 <ohm.pce21@sot.pdpu.ac.in>
2023-10-22 14:31:13 +0530

    add sidepanel & editor

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // Add this to your existing script.js
 
-document.getElementById("create-note-button").addEventListener("click", function () {
-    // Check if there are unsaved notes
-    const unsavedNotes = document.querySelectorAll(".note-box:not(.saved)");
+// document.getElementById("create-note-button").addEventListener("click", function () {
+//     // Check if there are unsaved notes
+//     const unsavedNotes = document.querySelectorAll(".note-box:not(.saved)");
 
-    if (unsavedNotes.length > 0) {
-        const confirmSave = confirm("Do you want to save your unsaved notes before leaving?");
+//     if (unsavedNotes.length > 0) {
+//         const confirmSave = confirm("Do you want to save your unsaved notes before leaving?");
 
-        if (confirmSave) {
-            // You can add code here to save the notes
-            alert("Notes saved!");
-        }
-    }
-    // Navigate to the home page
-    window.location.href = "home.html"; // Replace with your home page URL
-});
+//         if (confirmSave) {
+//             // You can add code here to save the notes
+//             alert("Notes saved!");
+//         }
+//     }
+//     // Navigate to the home page
+//     window.location.href = "home.html"; // Replace with your home page URL
+// });
+
 const parentItems = document.querySelectorAll('.Foldername');
 parentItems.forEach((parent) => {
     parent.addEventListener('click', function () {
@@ -27,3 +28,11 @@ parentItems.forEach((parent) => {
 });
 
 
+const editorElement = document.getElementById('editor');
+var editor = new Jodit(editorElement, {
+    height: 500,
+    maxheight: 500,
+    allowResizeX: false,
+    allowResizeY: false,
+    scrollY: true,
+});
